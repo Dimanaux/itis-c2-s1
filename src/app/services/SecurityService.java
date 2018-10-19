f47ea38c49d81d9923979535d6cf92ab6ff25039
Dmitry Barskov <barskoidim@gmail.com>
2018-10-19 12:35:00 +0300

    add security service

---
+++ src/app/services/SecurityService.java
@@ -0,0 +1,23 @@
+package app.services;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+class SecurityService {
+//    private static final byte[] SALT = new byte[] { 100, 16, -85, -9, -32, -81, -85, 109, 44, -10, -89, 62, -65, 42, 111, -32 };
+    private String hashPassword(String password) {
+        MessageDigest md;
+        try {
+            md = MessageDigest.getInstance("SHA-512");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            throw new RuntimeException("No sha-512 algorithm.");
+        }
+        md.update(new byte[] { 100, 16, -85, -9, -32, -81, -85, 109, 44, -10, -89, 62, -65, 42, 111, -32 });
+
+        byte[] hashedPassword = md.digest(password.getBytes(StandardCharsets.UTF_8));
+
+        return new String(hashedPassword, StandardCharsets.US_ASCII);
+    }
+}
