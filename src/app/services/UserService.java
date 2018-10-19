cecad21fe3266999fe3cd9756b215134df05c216
Dmitry Barskov <barskoidim@gmail.com>
2018-10-19 13:06:21 +0300

    create UserService

---
+++ src/app/services/UserService.java
@@ -0,0 +1,11 @@
+package app.services;
+
+public class UserService {
+    private SecurityService securityService;
+
+    public UserService() {
+        this.securityService = new SecurityService();
+    }
+
+
+}
