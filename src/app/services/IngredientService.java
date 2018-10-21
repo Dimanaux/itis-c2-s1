93e3841bd52f2b4f29a8e8bbda28b53274ee64ac
Dmitry Barskov <barskoidim@gmail.com>
2018-10-21 18:52:33 +0300

    implement methods in IngredientService

--- src/app/services/IngredientService.java
+++ src/app/services/IngredientService.java
@@ -1,11 +1,25 @@
 package app.services;
 
+import app.db.dao.IngredientDao;
 import app.db.models.Ingredient;
 
 import java.util.List;
 
 public class IngredientService {
+
+    private IngredientDao ingredientDao;
+
+    public IngredientService() {
+        this.ingredientDao = new app.db.dao.postgres.IngredientDao();
+    }
+
     public List<Ingredient> getAllIngredients() {
-        return null;
+        return ingredientDao.getAll();
+    }
+
+    public Ingredient create(String name) {
+        Ingredient ingredient = new Ingredient();
+        ingredient.setName(name);
+        return ingredientDao.save(ingredient);
     }
 }
