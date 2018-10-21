6029abe09ff31d2fe083f1ac9808feaff63654a7
Dmitry Barskov <barskoidim@gmail.com>
2018-10-21 18:55:13 +0300

    implement doPost for Ingredients Index

--- src/app/servlets/ingredients/IngredientsIndexServlet.java
+++ src/app/servlets/ingredients/IngredientsIndexServlet.java
@@ -12,10 +12,20 @@ import java.util.List;
 
 @WebServlet(name = "IngredientsIndexServlet", urlPatterns = {"/ingredients"})
 public class IngredientsIndexServlet extends AbstractIngredientsServlet {
+    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        User user = getUserService().authenticate(req);
+        if (user == null) {
+            resp.sendError(403);
+            return;
+        }
 
+        String ingredient_name = req.getParameter("ingredient_name");
+        getIngredientService().create(ingredient_name);
+        resp.sendRedirect("/ingredients");
     }
 
+    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         List<Ingredient> ingredients = getIngredientService().getAllIngredients();
         User user = getUserService().authenticate(req);
