5367e0b988192594e1644702b80f790595833112
Dmitry Barskov <barskoidim@gmail.com>
2018-10-21 17:15:53 +0300

    make Dao::save return Model

--- src/app/db/dao/Dao.java
+++ src/app/db/dao/Dao.java
@@ -5,7 +5,7 @@ import app.db.models.Model;
 import java.util.List;
 
 public interface Dao<M extends Model> {
-    boolean save(M model);
+    M save(M model);
     List<M> getAll();
     M getById(int id);
     boolean update(M newModel);
