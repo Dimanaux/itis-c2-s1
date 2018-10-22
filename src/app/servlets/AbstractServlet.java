73ec2b0208c6bca4bcbcf57e55ceb0790a7d7c3d
Dmitry Barskov <barskoidim@gmail.com>
2018-10-22 15:10:04 +0300

    move much of initialization to AbstractServlet

--- src/app/servlets/AbstractServlet.java
+++ src/app/servlets/AbstractServlet.java
@@ -1,6 +1,28 @@
 package app.servlets;
 
+import app.services.UserService;
+import app.util.Helper;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
 public abstract class AbstractServlet extends HttpServlet {
+    private Helper helper;
+    private UserService userService;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        helper = new Helper(getServletContext());
+        userService = new UserService();
+    }
+
+    public Helper getHelper() {
+        return helper;
+    }
+
+    public UserService getUserService() {
+        return userService;
+    }
 }
