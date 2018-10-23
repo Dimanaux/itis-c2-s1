9fb1dce3787e8ee5dc6ff5d12b2f96c1b5319fe6
Dmitry Barskov <barskoidim@gmail.com>
2018-10-23 21:51:29 +0300

    create logout servlet

---
+++ src/app/servlets/auth/LogoutServlet.java
@@ -0,0 +1,17 @@
+package app.servlets.auth;
+
+import app.servlets.AbstractServlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "LogoutServlet", urlPatterns = {"/logout"})
+public class LogoutServlet extends AbstractServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        getUserService().logout(req, resp);
+        resp.sendRedirect("/auth");
+    }
+}
