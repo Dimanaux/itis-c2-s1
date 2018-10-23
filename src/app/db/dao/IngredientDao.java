3621f9744708ed39b4cc33a88c7159e9f092b36b
Dmitry Barskov <barskoidim@gmail.com>
2018-10-23 21:58:45 +0300

    add methods to IngredientDao

--- src/app/db/dao/IngredientDao.java
+++ src/app/db/dao/IngredientDao.java
@@ -1,7 +1,12 @@
 package app.db.dao;
 
 import app.db.models.Ingredient;
+import app.db.models.Recipe;
+
+import java.util.List;
 
 public interface IngredientDao extends Dao<Ingredient> {
     Ingredient getByName(String name);
+    List<Ingredient> getByRecipe(Recipe recipe);
+    List<Ingredient> getByRecipeId(int recipeId);
 }
