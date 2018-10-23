53ec2617fd04b96bd02fef123283ffb3c5280fb0
Dmitry Barskov <barskoidim@gmail.com>
2018-10-23 22:03:20 +0300

    replace wrong method call

--- src/app/servlets/profile/ProfileIndexServlet.java
+++ src/app/servlets/profile/ProfileIndexServlet.java
@@ -1,25 +1,46 @@
 package app.servlets.profile;
 
+import app.db.dao.postgres.RecipeDao;
+import app.db.models.Recipe;
 import app.db.models.User;
 import app.servlets.AbstractServlet;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 
 @WebServlet(name = "ProfileIndexServlet", urlPatterns = {"/profile"})
 public class ProfileIndexServlet extends AbstractServlet {
+    private RecipeDao recipeDao;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        recipeDao = new RecipeDao();
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        User user = getUserService().authenticate(req);
+        User user = getUserService().getCurrentUser(req);
+
+        if (user == null) {
+            resp.sendRedirect("/auth");
+            return;
+        }
+
+        List<Recipe> recipes = recipeDao.getByAuthor(user);
 
         getHelper().render(
                 resp,
                 "Profile.ftl",
                 new HashMap<>() {{
                     put("user", user);
+                    put("recipes", recipes);
                 }}
         );
     }
