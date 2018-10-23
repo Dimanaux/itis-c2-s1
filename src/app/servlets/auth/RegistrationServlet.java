ed0652b76db75680b2f10171e171aef1d6c01430
Dmitry Barskov <barskoidim@gmail.com>
2018-10-23 22:02:50 +0300

    add redirect after registration, replace wrong method call

--- src/app/servlets/auth/RegistrationServlet.java
+++ src/app/servlets/auth/RegistrationServlet.java
@@ -11,13 +11,14 @@ import java.util.HashMap;
 
 @WebServlet(name = "RegistrationServlet", urlPatterns = {"/registration"})
 public class RegistrationServlet extends AbstractServlet {
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String username = req.getParameter("username");
         String password = req.getParameter("password");
         String name = req.getParameter("name");
 
         User user = getUserService().createUser(username, password, name);
         getUserService().authorize(req, user);
+        resp.sendRedirect("/profile");
     }
 
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
