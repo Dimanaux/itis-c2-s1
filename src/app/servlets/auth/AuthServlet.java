cfc4af72b4bd495d15f5685b61dc2343fba607e0
Dmitry Barskov <barskoidim@gmail.com>
2018-10-23 11:34:22 +0300

    wip on auth

--- src/app/servlets/auth/AuthServlet.java
+++ src/app/servlets/auth/AuthServlet.java
@@ -1,20 +1,39 @@
 package app.servlets.auth;
 
+import app.db.models.User;
 import app.servlets.AbstractServlet;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
 
 @WebServlet(name = "AuthServlet", urlPatterns = {"/auth"})
 public class AuthServlet extends AbstractServlet {
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        getUserService().authorize(req);
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        User user = getUserService().authenticate(req);
+        if (user == null) {
+            resp.sendError(400, "Wrong username or password.");
+        } else {
+            getUserService().authorize(req, user);
+            if (req.getParameter("remember_me") != null) {
+                getUserService().remember(resp, user);
+            }
+            resp.sendRedirect("/profile");
+        }
     }
 
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        User currentUser = getUserService().getCurrentUser(req);
+        if (currentUser != null) {
+            resp.sendRedirect("/profile");
+        } else {
+            getHelper().render(
+                    resp,
+                    "Auth.ftl",
+                    new HashMap<>()
+            );
+        }
     }
 }
--- src/app/servlets/auth/RegistrationServlet.java
+++ src/app/servlets/auth/RegistrationServlet.java
@@ -1,20 +1,35 @@
 package app.servlets.auth;
 
+import app.db.models.User;
 import app.servlets.AbstractServlet;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
 
-@WebServlet(name = "RegistrationServlet", urlPatterns = {"registration"})
+@WebServlet(name = "RegistrationServlet", urlPatterns = {"/registration"})
 public class RegistrationServlet extends AbstractServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        String name = req.getParameter("name");
 
+        User user = getUserService().createUser(username, password, name);
+        getUserService().authorize(req, user);
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        User currentUser = getUserService().getCurrentUser(req);
+        if (currentUser != null) {
+            resp.sendRedirect("/profile");
+        } else {
+            getHelper().render(
+                    resp,
+                    "Registration.ftl",
+                    new HashMap<>()
+            );
+        }
     }
 }
